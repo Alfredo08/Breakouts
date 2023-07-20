eb01c1b4c7866d23667b01685ac5b36c7d47314f
Alfredo08 <alfredosv08@gmail.com>
2023-07-19 18:59:47 -0600

    Adding news api

--- jQuery + AJAX Breakout/README.md
+++ jQuery + AJAX Breakout/README.md
@@ -1,5 +1,5 @@
 # Breakout - jQuery + AJAX
-[Github Repository](https://github.com/Alfredo08/Breakouts/tree/main/jQuery%20%2B%20AJAX%20Breakout) | [Vimeo Video Recording](https://vimeo.com/838509332/450dbd5a5b?share=copy)
+[Github Repository](https://github.com/Alfredo08/Breakouts/tree/main/jQuery%20%2B%20AJAX%20Breakout) | [Vimeo Video Recording](https://vimeo.com/846799520/f33b02b76c?share=copy)
 
 ## Topics to cover
 * [X] What is jQuery and AJAX?
@@ -7,5 +7,5 @@
 * [X] Demonstration
 
 ## Documentation links
-* [Dog CEO API](https://dog.ceo/dog-api/documentation/)
 * [jQuery Docs](https://api.jquery.com/)
+* [News API Docs](https://newsapi.org/)
--- jQuery + AJAX Breakout/js/index.js
+++ jQuery + AJAX Breakout/js/index.js
@@ -1,28 +1,46 @@
+const BASE_URL = "https://newsapi.org";
+const token = "Place your API token here!";
 
-$('#dogAPIForm').on('submit', (event) => {
+$newsForm = $('.news-form');
+
+$newsForm.on('submit', (event) => {
     event.preventDefault();
-    
-    const numberOfDogs = $('#numberOfDogs').val();
-    const url = `https://dog.ceo/api/breeds/image/random/${numberOfDogs}`;
-    
-    /*
-    $.get(url, (data) => {
-        $('.results').empty();
-        for (let i = 0; i < data.message.length; i ++){
-            console.log(data.message[i]);
-            $('.results').append(`<img class="dogImage" src="${data.message[i]}" alt="Random dog image">`);
-        }
-    }); */
+    $searchText = $('#search-text').val();
+
+    const queryParams = {
+        //apiKey : token,
+        q : $searchText,
+        pageSize : 20 
+    };
 
     $.ajax({
-        url: url,
-        method: "GET" 
-    }).done((data) => {
-        $('.results').empty();
-        for (let i = 0; i < data.message.length; i ++){
-            console.log(data.message[i]);
-            $('.results').append(`<img class="dogImage" src="${data.message[i]}" alt="Random dog image">`);
+        url : `${BASE_URL}/v2/everything`,
+        data : queryParams,
+        method : "GET",
+        headers : {
+            "X-Api-Key" : token
+        },
+        success : (responseJSON) => {
+            $('.results').empty();
+            responseJSON.articles.forEach((article) => {
+                $('.results').append(`
+                    <div class="article">
+                        <h2 class="article-title"> ${article.title} </h2>
+                        <div class="article-image">
+                            <img src="${article.urlToImage}" alt="${article.title}" >
+                        </div>
+                        <p class="article-author"> ${article.author} </p>
+                        <p class="article-description"> ${article.description} </p>
+                        <p> 
+                            <a target="_blank" href="${article.url}"> Take me to the article </a> 
+                        </p>
+                    </div>
+                `);
+            });
+        },
+        error : (error) => {
+            console.log(error);
         }
     });
-
+    
 });
